759cd4d393284b24fac9a7d9f43f74279bbf054b
mariaperez08 <mperez2@instituticaria.cat>
2024-10-23 13:45:51 +0200

    sego enviament

--- divisio.py
+++ divisio.py
@@ -2,6 +2,6 @@ divisor = int(input("introdueix el teu divisor(nombre enter)"))
 divident = int(input("introdueix el teu divient(nombre enter)"))
 quocient= divident // divisor 
 residu = divident % divisor
+print(f"Divisio = {divident}/{divisor}")
 print(quocient)
 print(residu)
-print(f"Divisió = {divident}/{divisor}")
