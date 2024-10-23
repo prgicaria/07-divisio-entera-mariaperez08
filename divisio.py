fd2fab9211fdf533e1901320811a7afa56c336b3
mariaperez08 <mperez2@instituticaria.cat>
2024-10-23 13:52:38 +0200

    tercer intent

--- divisio.py
+++ divisio.py
@@ -1,7 +1,9 @@
-divisor = int(input("introdueix el teu divisor(nombre enter)"))
 divident = int(input("introdueix el teu divient(nombre enter)"))
+divisor = int(input("introdueix el teu divisor(nombre enter)"))
 quocient= divident // divisor 
 residu = divident % divisor
 print(f"Divisio = {divident}/{divisor}")
 print(quocient)
 print(residu)
+
+
